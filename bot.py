48244ddd478dadd5f513b3f95a0c3bb9c4d21b5a
Tron Schell <zoniixyt@gmail.com>
2018-12-07 13:15:02 -0500

    Added full catalog of BTC coins in Tradeogre

--- bot.py
+++ bot.py
@@ -2,15 +2,7 @@ import discord, json, asyncio, requests
 from discord.ext.commands import Bot
 
 
-currencies = ['BTC-ACM','BTC-AEON', 'BTC-ARQ', 'BTC-BBS', 'BTC-BCN', 'BTC-BCN', 'BTC-BKC', 'BTC-BLOC', 'BTC-BSM', 'BTC-BTCP', 'BTC-CIV']
-
-help_text = 
-'''
-    !btc{TICKER} - Gives Price, High, Low , and Asking price of inputed Ticker.
-
-
-
-'''
+currencies = ['BTC-ACM','BTC-AEON', 'BTC-ARQ', 'BTC-BBS', 'BTC-BCN', 'BTC-BCN', 'BTC-BKC', 'BTC-BLOC', 'BTC-BSM', 'BTC-BTCP', 'BTC-CIV', 'BTC-COAL', 'BTC-D', 'BTC-DASH', 'BTC-DERO', 'BTC-DOGE', 'BTC-ETH', 'BTC-ETN', 'BTC-ETNX', 'BTC-ETNXP', 'BTC-FBF', 'BTC-GPKR', 'BTC-GRFT', 'BTC-INC', 'BTC-INTU', 'BTC-IRD', 'BTC-KRB', 'BTC-LNS', 'BTC-LOKI', 'BTC-LTC', 'BTC-LTHN', 'BTC-LUX', 'BTC-MSR', 'BTC-NAH', 'BTC-NBR', 'BTC-OMB', 'BTC-PCN', 'BTC-PIVX', 'BTC-PIVX', 'BTC-PLURA', 'BTC-PURK', 'BTC-QTUM', 'BTC-QUAN', 'BTC-RTO', 'BTC-RVN', 'BTC-RYO', 'BTC-SHB', 'BTC-SLD', 'BTC-SOLACE', 'BTC-SUMO', 'BTC-SUQA', 'BTC-TRTL', 'BTC-TUBE', 'BTC-WAE', 'BTC-WOW', "BTC-WTIP", 'BTC-XAO', 'BTC-XGS', 'BTC-XHV', 'BTC-XMC', 'BTC-XMR', 'BTC-XMV', 'BTC-XNV', 'BTC-XPP', 'BTC-XRN', 'BTC-XTA', 'BTC-XTL', 'BTC-XTRI', 'BTC-XUN', 'BTC-XVG', 'BTC-ZEL']
 
 def getPrice(currency, crypto):
 

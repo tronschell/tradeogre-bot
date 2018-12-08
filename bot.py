8d44c5db85f70d47d9025137e837a8dfb0997c24
unknown <zoniixyt@gmail.com>
2018-12-08 18:56:45 -0500

    Added more effective code so I don't have to hard code every single pair

--- bot.py
+++ bot.py
@@ -5,7 +5,25 @@ from discord.ext.commands import Bot
 currencies = ['BTC-ACM','BTC-AEON', 'BTC-ARQ', 'BTC-BBS', 'BTC-BCN', 'BTC-BCN', 'BTC-BKC', 'BTC-BLOC', 'BTC-BSM', 'BTC-BTCP', 'BTC-CIV', 'BTC-COAL', 'BTC-D', 'BTC-DASH', 'BTC-DERO', 'BTC-DOGE', 'BTC-ETH', 'BTC-ETN', 'BTC-ETNX', 'BTC-ETNXP', 'BTC-FBF', 'BTC-GPKR', 'BTC-GRFT', 'BTC-INC', 'BTC-INTU', 'BTC-IRD', 'BTC-KRB', 'BTC-LNS', 'BTC-LOKI', 'BTC-LTC', 'BTC-LTHN', 'BTC-LUX', 'BTC-MSR', 'BTC-NAH', 'BTC-NBR', 'BTC-OMB', 'BTC-PCN', 'BTC-PIVX', 'BTC-PIVX', 'BTC-PLURA', 'BTC-PURK', 'BTC-QTUM', 'BTC-QUAN', 'BTC-RTO', 'BTC-RVN', 'BTC-RYO', 'BTC-SHB', 'BTC-SLD', 'BTC-SOLACE', 'BTC-SUMO', 'BTC-SUQA', 'BTC-TRTL', 'BTC-TUBE', 'BTC-WAE', 'BTC-WOW', "BTC-WTIP", 'BTC-XAO', 'BTC-XGS', 'BTC-XHV', 'BTC-XMC', 'BTC-XMR', 'BTC-XMV', 'BTC-XNV', 'BTC-XPP', 'BTC-XRN', 'BTC-XTA', 'BTC-XTL', 'BTC-XTRI', 'BTC-XUN', 'BTC-XVG', 'BTC-ZEL']
 
 def getPrice(currency, crypto):
+    input1 = currency.upper()
+    input2 = crypto.upper()
 
+
+    if(currency.upper() in currencies):
+        url = "https://tradeogre.com/api/v1/ticker/{}-{}".format(input1, input2)
+        response = requests.get(url)
+        data = response.json()
+
+        btc_acm_price = data['price']
+        return btc_acm_price
+    else:
+        return "Not a supprted currency"
+
+
+
+
+
+print(getPrice("btc", "acm"))
 
 url = "https://tradeogre.com/api/v1/ticker/BTC-ACM"
 response = requests.get(url)

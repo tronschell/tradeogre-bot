a5f0bd3f4ced0dc719d9c5ca46dccbeb97632477
Tron Schell <zoniixyt@gmail.com>
2018-12-08 21:18:09 -0500

    Added more functionality

--- bot.py
+++ bot.py
@@ -2,18 +2,24 @@ import discord, json, asyncio, requests
 from discord.ext.commands import Bot
 
 
-currencies = ['BTC-ACM','BTC-AEON', 'BTC-ARQ', 'BTC-BBS', 'BTC-BCN', 'BTC-BCN', 'BTC-BKC', 'BTC-BLOC', 'BTC-BSM', 'BTC-BTCP', 'BTC-CIV', 'BTC-COAL', 'BTC-D', 'BTC-DASH', 'BTC-DERO', 'BTC-DOGE', 'BTC-ETH', 'BTC-ETN', 'BTC-ETNX', 'BTC-ETNXP', 'BTC-FBF', 'BTC-GPKR', 'BTC-GRFT', 'BTC-INC', 'BTC-INTU', 'BTC-IRD', 'BTC-KRB', 'BTC-LNS', 'BTC-LOKI', 'BTC-LTC', 'BTC-LTHN', 'BTC-LUX', 'BTC-MSR', 'BTC-NAH', 'BTC-NBR', 'BTC-OMB', 'BTC-PCN', 'BTC-PIVX', 'BTC-PIVX', 'BTC-PLURA', 'BTC-PURK', 'BTC-QTUM', 'BTC-QUAN', 'BTC-RTO', 'BTC-RVN', 'BTC-RYO', 'BTC-SHB', 'BTC-SLD', 'BTC-SOLACE', 'BTC-SUMO', 'BTC-SUQA', 'BTC-TRTL', 'BTC-TUBE', 'BTC-WAE', 'BTC-WOW', "BTC-WTIP", 'BTC-XAO', 'BTC-XGS', 'BTC-XHV', 'BTC-XMC', 'BTC-XMR', 'BTC-XMV', 'BTC-XNV', 'BTC-XPP', 'BTC-XRN', 'BTC-XTA', 'BTC-XTL', 'BTC-XTRI', 'BTC-XUN', 'BTC-XVG', 'BTC-ZEL']
+yes = ['BTC', 'LTC']
 
-client = Bot(command_prefix = "$")
-TOKEN = ""
+currencies = ['ACM','AEON', 'ARQ', 'BBS', 'BCN', 'BKC', 'BLOC', 'BSM', 'BTCP', 'CIV',
+'COAL', 'D', 'DASH', 'DERO', 'DOGE', 'ETH', 'ETN', 'ETNX', 'ETNXP', 'FBF', 'GPKR', 'GRFT',
+'INC', 'INTU', 'IRD', 'KRB', 'LNS', 'LOKI', 'LTC', 'LTHN', 'LUX', 'MSR', 'NAH', 'NBR', 'OMB',
+'PCN', 'PIVX', 'PIVX', 'PLURA', 'PURK', 'QTUM', 'QUAN', 'RTO', 'RVN', 'RYO', 'SHB', 'SLD', 'SOLACE',
+'SUMO', 'SUQA', 'TRTL', 'TUBE', 'WAE', 'WOW', "WTIP", 'XAO', 'XGS', 'XHV', 'XMC', 'XMR', 'XMV', 
+'XNV', 'XPP', 'XRN', 'XTA', 'XTL', 'XTRI', 'XUN', 'XVG', 'ZEL']
+
+client = Bot(command_prefix = "!")
+TOKEN = "NTIxMTM0ODI5MjE2MDcxNzMw.Du4EOg.ypV2JbmHXg3uAGUozLAI9w3VR98"
 
 help_text:'''
 
-!price {BTC/LTC}{TICKER} - gives price data including: high, low and current price.
+!price {BTC/LTC}{TICKER} - gets current price data including: high, low and current price.
 !donate - gives developer Bitcoin address
 
 '''
-
 
 
 @client.event
@@ -23,24 +29,34 @@ async def on_ready():
 
 @client.command()
 async def price(currency, crypto):
-    try:
-        input1 = currency.upper()
-        input2 = crypto.upper()
 
-        if(currency.upper() in currencies):
-            url = "https://tradeogre.com/api/v1/ticker/{}-{}".format(input1, input2)
-            response = requests.get(url)
-            data = response.json()
+    input1 = currency.upper()
+    input2 = crypto.upper()
 
-            btc_acm_price = data['price']
-            return btc_acm_price
-        else:
-            return "Not a supported currency"
-    except:
-        return "Error"
+    if(input1 in yes&& input2 in currencies):
+        url = "https://tradeogre.com/api/v1/ticker/{}-{}".format(input1, input2)
+        response = requests.get(url)
+        data = response.json()
 
+        price_data = data['price']
+        high_data = data['high']
+        low_data = data['low']
+            
+        finalString = "Current Price: " + price_data + "24hr High: " + high_data + "24hr Low: " + low_data
+
+        embed = discord.Embed(title='{}-{}'.format(currency, crypto), description=finalString, color =0x00ff00)
+
+        await client.say(finalString)
+    else:
+        await client.say("Test")
+
+@client.command()
 async def donate():
-    client.say("To donate please use this BTC address: ")
+    await client.say("To donate please use this BTC address: ")
+
+@client.command()
+async def api():
+    await client.say("We are using the public Tradeogre API: https://tradeogre.com/api/v1")
 
 
 
